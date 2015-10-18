b71e8dbf086b76ccba8d52283ec1676833955328
Jose-Luis Rivas <me@ghostbar.co>
2015-10-18 05:07:39 +0000

    update index.html s

--- listing.sh
+++ listing.sh
@@ -2,8 +2,8 @@
 
 for DIR in $(find ./ -type d); do
   (
-    echo  "<html>\n<body>\n<h1>Directory listing</h1>\n<hr/>\n<pre>"
+    echo  "<html><body><h1>Directory listing</h1><hr/><pre>"
     ls -1pa "${DIR}" | grep -v "^\./$" | grep -v "^index\.html$" | awk '{ printf "<a href=\"%s\">%s</a>\n",$1,$1 }'
-    echo  "</pre>\n</body>\n</html>"
+    echo  "</pre></body></html>"
   ) > "${DIR}/index.html"
 done
